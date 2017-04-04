bb3981adecd245d1556a16c336bdac19f3863b5c
Alex Poley <alex.poley14@gmail.com>
2017-04-04 13:58:01 +0200

    xml is loaded

---
+++ DownloadApp/DownloadApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DownloadApp
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            //determine url to connect to
+            string url = "https://commons.wikimedia.org/w/api.php?action=featuredfeed&feed=potd&format=json";
+            
+            DownloadWallpaper().Wait();
+            GetProductAsync("?action=featuredfeed&feed=potd&format=json").Wait();
+
+            Console.Write("Press enter to exit...");
+            Console.ReadLine();
+        }
+
+        static async Task DownloadWallpaper()
+        {
+            // New code:
+            client.BaseAddress = new Uri("https://commons.wikimedia.org/w/api.php");
+            client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        static async Task<Wallpaper> GetProductAsync(string path)
+        {
+            Wallpaper wallpaper = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(content);
+                try
+                {
+                    XmlElement firstItem = doc.FirstChild.NextSibling["channel"]["item"];
+                    string link = firstItem["link"].InnerText;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return wallpaper;
+        }
+    }
+}
---
+++ DownloadApp/DownloadApp/Wallpaper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadApp
+{
+    class Wallpaper
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public DateTime DatePosted { get; set; }
+        public DateTime DateDownloaded { get; set; }
+
+        public Wallpaper(string url)
+        {
+            Url = url;
+            DateDownloaded = DateTime.Today;
+        }
+    }
+}
